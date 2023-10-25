8e31991bfb0405214d0da7a7a8147b8e169b2fc2
1Max58 <>
2023-10-25 16:18:41 +0300

    User input and BMI index calculation implemented

--- src/com/example/bmiindexcalculation/BmiIndexCalculation.java
+++ src/com/example/bmiindexcalculation/BmiIndexCalculation.java
@@ -18,5 +18,36 @@ public class BmiIndexCalculation {
 
         float bmiIndex = (float) (weight / Math.pow(height / 100, 2));
         System.out.println("BMI index = " + bmiIndex);
+
+        if (bmiIndex < 16.00) {
+            System.out.println("16.00 - starvation");
+        }
+        else if (bmiIndex < 17.00) {
+            System.out.println("16.00 - 16.99 - emaciation");
+        }
+        else if (bmiIndex < 18.50) {
+            System.out.println("17.00 - 18.49 - underweight");
+        }
+        else if (bmiIndex < 23.00) {
+            System.out.println("18.50 - 22.99 - normal, low range");
+        }
+        else if (bmiIndex < 25.00) {
+            System.out.println("23.00 - 24.99 - normal, high range");
+        }
+        else if (bmiIndex < 27.50) {
+            System.out.println("25.00 - 27.49 - overweight, low range");
+        }
+        else if (bmiIndex < 30) {
+            System.out.println("27.50 - 29.99 - overweight, high range");
+        }
+        else if (bmiIndex < 35) {
+            System.out.println("30 - 34.9 - 1st degree obesity");
+        }
+        else if (bmiIndex < 40 ) {
+            System.out.println("35 - 39.9 - 2nd degree obesity");
+        }
+        else {
+            System.out.println("40 - 3rd degree obesity");
+        }
     }
 }
